49160d05e7f635e3f2a79e35ac2e47680f428cd0
Haikal Iskandar <haikalultraman123@gmail.com>
2024-12-19 15:29:19 +0800

    v0.2.1

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,4 +1,11 @@
+locals {
+  bucket_created_file = "bucket_created.txt"
+  first_run           = !fileexists(local.bucket_created_file)
+}
+
 resource "google_storage_bucket" "website_bucket" {
+  count = local.first_run ? 1 : 0
+
   name          = "sispaa-revamp-website-bucket" # Replace with your bucket name
   location      = "asia-northeast1"
   force_destroy = true
@@ -6,28 +13,32 @@ resource "google_storage_bucket" "website_bucket" {
   website {
     main_page_suffix = "index.html"
   }
+
+  provisioner "local-exec" {
+    command = "echo 'Bucket created' > ${local.bucket_created_file}"
+  }
 }
 
 resource "google_storage_bucket_object" "html" {
   name   = "index.html"
-  bucket = google_storage_bucket.website_bucket.name
+  bucket = local.first_run ? google_storage_bucket.website_bucket[0].name : "sispaa-revamp-website-bucket"
   source = "../src/index.html"
 }
 
 resource "google_storage_bucket_object" "js" {
   name   = "app.js"
-  bucket = google_storage_bucket.website_bucket.name
+  bucket = local.first_run ? google_storage_bucket.website_bucket[0].name : "sispaa-revamp-website-bucket"
   source = "../src/app.js"
 }
 
 resource "google_storage_bucket_object" "css" {
   name   = "style.css"
-  bucket = google_storage_bucket.website_bucket.name
+  bucket = local.first_run ? google_storage_bucket.website_bucket[0].name : "sispaa-revamp-website-bucket"
   source = "../src/style.css"
 }
 
 resource "google_storage_bucket_iam_member" "public_access" {
-  bucket = google_storage_bucket.website_bucket.name
+  bucket = local.first_run ? google_storage_bucket.website_bucket[0].name : "sispaa-revamp-website-bucket"
   role   = "roles/storage.objectViewer"
   member = "allUsers"
 }
