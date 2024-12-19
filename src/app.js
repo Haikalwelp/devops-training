76fde2f6cf93ebd2e68393df4fa7e690c6aac912
Haikal Iskandar <haikalultraman123@gmail.com>
2024-12-19 15:20:06 +0800

    v0.1.9.1 - minor fixes

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ function greet(names) {
 
 document.addEventListener('DOMContentLoaded', () => {
   const contentDiv = document.getElementById('content');
-  contentDiv.textContent = 'Helo Belo!';
+  contentDiv.textContent = 'Helo BelLo!';
 });
 
 module.exports = { greet };
