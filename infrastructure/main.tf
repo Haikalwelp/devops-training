b8be7f2789a75507b4c412810ddf1b41c5c9f7d3
Haikal Iskandar <haikalultraman123@gmail.com>
2024-12-19 15:22:56 +0800

    v0.1.10

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,7 +1,3 @@
-data "google_storage_bucket" "website_bucket" {
-  name     = "sispaa-revamp-website-bucket" # Replace with your bucket name
-}
-
 resource "google_storage_bucket" "website_bucket" {
   name          = "sispaa-revamp-website-bucket" # Replace with your bucket name
   location      = "asia-northeast1"
@@ -14,24 +10,24 @@ resource "google_storage_bucket" "website_bucket" {
 
 resource "google_storage_bucket_object" "html" {
   name   = "index.html"
-  bucket = data.google_storage_bucket.website_bucket.name
+  bucket = google_storage_bucket.website_bucket.name
   source = "../src/index.html"
 }
 
 resource "google_storage_bucket_object" "js" {
   name   = "app.js"
-  bucket = data.google_storage_bucket.website_bucket.name
+  bucket = google_storage_bucket.website_bucket.name
   source = "../src/app.js"
 }
 
 resource "google_storage_bucket_object" "css" {
   name   = "style.css"
-  bucket = data.google_storage_bucket.website_bucket.name
+  bucket = google_storage_bucket.website_bucket.name
   source = "../src/style.css"
 }
 
 resource "google_storage_bucket_iam_member" "public_access" {
-  bucket = data.google_storage_bucket.website_bucket.name
+  bucket = google_storage_bucket.website_bucket.name
   role   = "roles/storage.objectViewer"
   member = "allUsers"
 }
