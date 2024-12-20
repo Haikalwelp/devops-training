a5228b4f5599927a938a4bd7f67903117068207a
Haikal Iskandar <haikalultraman123@gmail.com>
2024-12-20 09:24:36 +0800

    v0.2.3.4

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ function greet(names) {
 
 document.addEventListener('DOMContentLoaded', () => {
   const contentDiv = document.getElementById('content');
-  contentDiv.textContent = 'Hello Belo!';
+  contentDiv.textContent = 'Real!';
 });
 
 module.exports = { greet };
